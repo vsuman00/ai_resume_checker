0665d08c8077ebb0cfc50f02150690c50723103a
vsuman00 <vaibhavsuman00@gmail.com>
2025-09-20 01:36:08 +0530

    Fetch real resumes on the Homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,7 +1,23 @@
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "~/lib/puter";
 
-const ResumeCard = ({ resume :{id, companyName, jobTitle, feedback, imagePath} } :{ resume: Resume }) => {
+const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
+    const { fs } = usePuterStore();
+    const [resumeUrl, setResumeUrl] = useState('');
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
@@ -14,17 +30,17 @@ const ResumeCard = ({ resume :{id, companyName, jobTitle, feedback, imagePath} }
                     <ScoreCircle score={feedback.overallScore} />
                 </div>
             </div>
-
-            <div className="gradient-border animate-in fade-in duration-1000">
-                <div className="w-full h-full">
-                    <img
-                        src={imagePath}
-                        alt="resume"
-                        className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-                    />
+            {resumeUrl && (
+                <div className="gradient-border animate-in fade-in duration-1000">
+                    <div className="w-full h-full">
+                        <img
+                            src={resumeUrl}
+                            alt="resume"
+                            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                        />
+                    </div>
                 </div>
-            </div>
-            
+            )}
         </Link>
     )
 }
